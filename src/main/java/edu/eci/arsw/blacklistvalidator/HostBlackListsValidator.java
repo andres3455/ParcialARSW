ef729644e9378a3b207724467efd3421ad63968c
Andres <andres.rchaparro@labinfo.is.escuelaing.edu.co>
2025-02-27 09:10:17 -0500

    ahora si funciona

--- src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListsValidator.java
+++ src/main/java/edu/eci/arsw/blacklistvalidator/HostBlackListsValidator.java
@@ -65,7 +65,57 @@ public class HostBlackListsValidator {
         return blackListOcurrences;
     }
 
-    private static final Logger LOG = Logger.getLogger(HostBlackListsValidator.class.getName());
 
+    // Nueva version del metodo CheckHost con el parametro adicional de N hilos 
+
+    public List<Integer> checkHost(String ipaddress, int N) {
+        LinkedList<Integer> blackListOcurrences = new LinkedList<>();
+
+        int ocurrencesCount = 0;
+
+        ArrayList<BlackLisThread> blackThread = new ArrayList<BlackLisThread>();
+
+        HostBlacklistsDataSourceFacade skds = HostBlacklistsDataSourceFacade.getInstance();
+
+        int checkedListsCount = 0;
+
+        @SuppressWarnings("unused")
+        ArrayList<BlackLisThread> blackThreads;
+
+        int spaces = skds.getRegisteredServersCount() / N;
+
+
+        /** Creacion de hilos con sus respectivos parametros */
+        for (int i = 0; i < N; i++) {
+            if (i < N - 1) {
+                blackThread.add(new BlackLisThread((i * spaces), ((i + 1) * spaces) - 1, ipaddress, skds));
+            } else if (i == N - 1) {
+                blackThread
+                        .add(new BlackLisThread((i * spaces), skds.getRegisteredServersCount(), ipaddress, skds));
+            }
+            blackThread.get(i).start();
+        }
+        for (BlackLisThread thread : blackThread) {
+            /** En caso que este vivo el hilo, continue con la busqueda */
+            while (thread.isAlive()) {
+                continue;
+            }
+            ocurrencesCount += thread.ipfoundonblacklist();
+            blackListOcurrences.addAll(thread.getBlackListOcurrence());
+            checkedListsCount += thread.getcountList();
+        }
+        if (ocurrencesCount >= BLACK_LIST_ALARM_COUNT) {
+            skds.reportAsNotTrustworthy(ipaddress);
+        } else {
+            skds.reportAsTrustworthy(ipaddress);
+        }
+
+        LOG.log(Level.INFO, "Checked Black Lists:{0} of {1}",
+                new Object[] { checkedListsCount, skds.getRegisteredServersCount() });
+
+        return blackListOcurrences;
+    }
+
+    private static final Logger LOG = Logger.getLogger(HostBlackListsValidator.class.getName());
 
 }
